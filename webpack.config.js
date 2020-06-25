a1f274ea667584ef891e0ca077f209e3b662a9b2
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-06-24 20:00:20 -0700

    add html loader and file loader to template

--- webpack.config.js
+++ webpack.config.js
@@ -32,11 +32,30 @@ module.exports = {
           'css-loader'
         ]
       },
-      {
+      { 
+      test: /\.(gif|png|jpe?g)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/images/'
+          }
+        }
+      ]
+},
+{
+  test:/\.html$/,
+  use: [
+    'html-loader'
+  ]
+},
+        {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
       }
+      
     ]
   }
 };
